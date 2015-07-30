ac306846edb6fd99a813b73f9e59e12d161b4cc5
f <f>
2015-07-30 16:41:05 +0300

    animation

--- src/scripts/app/entities/level/cell.js
+++ src/scripts/app/entities/level/cell.js
@@ -2,7 +2,6 @@
 
 import {CompositeMap} from 'app/util/composite-map';
 import Promise from 'bluebird';
-
 class Cell extends Phaser.Sprite {
   constructor(level, X, Y) {
     super(level.game);
@@ -45,25 +44,23 @@ class Cell extends Phaser.Sprite {
   }
 
   show() {
+    this.visible = true;
+    this.inputEnabled = true;
+    let showAlpha = this.game.add.tween(this).to({alpha: 1}, 500, Phaser.Easing.Quadratic.In, true);
+    let showScale = this.game.add.tween(this.scale).to({x: 1, y: 1}, 900, Phaser.Easing.Elastic.InOut);
     return new Promise((resolve) => {
-      this.visible = true;
-      this.inputEnabled = true;
-      this.game.add.tween(this)
-        .to({alpha: 1}, 500, Phaser.Easing.Quadratic.InOut, true)
-        .chain(this.game.add.tween(this.scale).to({x: 1, y: 1}, 900, Phaser.Easing.Elastic.InOut))
-        .onComplete.add(() => {
-          resolve();
-        });
+      showAlpha.chain(showScale);
+      showScale.onComplete.add(() => {
+        resolve();
+      });
     });
   }
 
   hide() {
-    let hideScale = this.game.add.tween(this.scale).to({x: .5, y: .5}, 900, Phaser.Easing.Elastic.InOut, true);
-    let hideAlpha = this.game.add.tween(this).to({alpha: 0}, 500, Phaser.Easing.Quadratic.InOut);
+    let hideScale = this.game.add.tween(this.scale).to({x: .5, y: .5}, 900, Phaser.Easing.Elastic.Out, true);
+    let hideAlpha = this.game.add.tween(this).to({alpha: 0}, 500, Phaser.Easing.Quadratic.In);
     return new Promise((resolve) => {
-      hideScale
-        .chain(hideAlpha);
-
+      hideScale.chain(hideAlpha);
       hideAlpha.onComplete.add(() => {
         this.visible = false;
         this.inputEnabled = false;
